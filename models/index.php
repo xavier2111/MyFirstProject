84d69e5001ab6b0992d94a590f3dedf2acba58b0
Administrator <Administrator@GEBRUIK-310QJCC>
2019-03-28 08:43:59 +0200

    Palindroom function

--- models/index.php
+++ models/index.php
@@ -1,10 +1,4 @@
 <?php
 include_once"./palindroom.php";
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 echo revertWord("piet");
